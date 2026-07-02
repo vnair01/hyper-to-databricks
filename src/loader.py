1b7328b0230b9926a2d9ea15d3b70cefb50e218e
Vineet Nair <60346339+vnair01@users.noreply.github.com>
2026-07-02 14:19:48 +0530

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,2 +1,8 @@
 def load_to_databricks(df, table_name):
-    df.write.mode("overwrite").saveAsTable(table_name)
+    df.write.format("delta") \
+        .mode("overwrite") \
+        .option("overwriteSchema", "true") \
+        .option("delta.columnMapping.mode", "name") \
+        .option("delta.minReaderVersion", "2") \
+        .option("delta.minWriterVersion", "5") \
+        .saveAsTable(TARGET_TABLE)
